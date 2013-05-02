e4214340acf204bf9fbfa15055ec38824d9a2ee5
Frank <Frank@Frank-PC>
2013-05-02 22:21:14 +0200

    Initial version

---
+++ src/pas/member/MemberController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pas.member;
+
+/**
+ *
+ * @author Frank
+ */
+public class MemberController {
+    
+}
